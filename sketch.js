f8f75091b5aa09f9601b3b0d9c5bd6e7a1576ec5
Shimona012 <bebubabu011@gmail.com>
2020-12-12 18:40:46 +0530

    4th piggy added

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var pig4;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -23,7 +24,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+//pig4= new Pig(800,300);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -58,7 +59,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+//pig4.display();
     bird.display();
     platform.display();
 }
